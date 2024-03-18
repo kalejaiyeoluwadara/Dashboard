8727a057871ef7ff68e03fae052003585239fa84
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-03-18 23:30:02 +0100

    first updates

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// export default App
+import React, { useState } from "react";
+import "./App.css";
+import Admin from "./Admin/Admin";
+const App = () => {
+  return (
+    <>
+      <Admin />
+    </>
+  );
+};
+export default App;
